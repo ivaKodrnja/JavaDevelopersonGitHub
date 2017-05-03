87f24d06018199eec618dad2ec32d5bc37364d27
ivaKodrnja <iva.kodrnja@gmail.com>
2017-05-03 13:18:39 +0200

    finished

--- app/src/main/java/com/kodrnja/javadevelopersongithub/MainActivity.java
+++ app/src/main/java/com/kodrnja/javadevelopersongithub/MainActivity.java
@@ -8,7 +8,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Build;
-import android.support.annotation.LayoutRes;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
